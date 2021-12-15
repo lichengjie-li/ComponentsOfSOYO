17b1c26dca58a4cf0fe2e37148dfda71c6c933d1
wuboluo <383049891@qq.com>
2021-12-15 18:54:49 +0800

    set AdapterValue returnValue to T

--- Assets/Scripts/Core/Tool/Adapter.cs
+++ Assets/Scripts/Core/Tool/Adapter.cs
@@ -1,20 +1,13 @@
-﻿using System;
-
-public static class Adapter
+﻿public static class Adapter
 {
     /// <summary>
     ///     根据一个 bool设置大于它或小于它时，传入的 value的值
     /// </summary>
-    /// <param name="value">需要被更改的值</param>
     /// <param name="accordingTo">判断条件</param>
     /// <param name="onTrue">True时的值</param>
     /// <param name="onFalse">False时的值</param>
-    /// <typeparam name="T"></typeparam>
-    /// <exception cref="ArgumentNullException"></exception>
-    public static void SetAdapterValue<T>(ref T value, bool accordingTo, T onTrue, T onFalse)
+    public static T SetAdapterValue<T>(bool accordingTo, T onTrue, T onFalse)
     {
-        if (value == null) throw new ArgumentNullException(nameof(value));
-
-        value = accordingTo ? onTrue : onFalse;
+        return accordingTo ? onTrue : onFalse;
     }
 }
--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -3,12 +3,11 @@
 public class TestScript : MonoBehaviour
 {
     public bool b;
-
     public int number;
 
     [ContextMenu("Test")]
     private void Test()
     {
-        Adapter.SetAdapterValue(ref number, b, -1, 1);
+        number = Adapter.SetAdapterValue(b, 1, -1);
     }
 }
